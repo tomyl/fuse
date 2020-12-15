5392161c4b8928da41edfbb4b1527a101a9eac4b
Tommy Lindgren <tommy.lindgren@zebware.com>
2020-12-15 16:50:03 +0100

    Increased maxWrite to 1024kB
    Linux 5.4.0 caps this value at 1024kB (FUSE_MAX_MAX_PAGES=256, 4kB pages).

--- fuse_linux.go
+++ fuse_linux.go
@@ -2,6 +2,7 @@ package fuse
 
 // Maximum file write size we are prepared to receive from the kernel.
 //
-// Linux 4.2.0 has been observed to cap this value at 128kB
-// (FUSE_MAX_PAGES_PER_REQ=32, 4kB pages).
-const maxWrite = 128 * 1024
+// Linux 5.4.0 caps this value at 1024kB (FUSE_MAX_MAX_PAGES=256, 4kB pages).
+// Default kernel value is 128kB (FUSE_DEFAULT_MAX_PAGES_PER_REQ=32, 4kB
+// pages).
+const maxWrite = 1024 * 1024
